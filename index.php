9e5b603cc86321aa4e80757cf37d83e2d70f54eb
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-02 00:15:22 -0600

    botones de paypal

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://www.paypal.com/sdk/js?client-id=AZfUU5uy4JNNgtbnwTOkZeicL8gO3f9GOZ03iRE1FKDVMeqB3DcUQ7Rm2QvNnF9rRiBNySF82QOEIGhB&currency=MXN"></script>
 </head>
 
 <body>
-    <script src="https://www.paypal.com/sdk/js?client-id=AZfUU5uy4JNNgtbnwTOkZeicL8gO3f9GOZ03iRE1FKDVMeqB3DcUQ7Rm2QvNnF9rRiBNySF82QOEIGhB&currency=MXN"></script>
+    <div id="paypal-button-container"></div>
+    <script>
+        paypal.buttons().render('#paypal-button.container');
+    </script>
 </body>
-<div id="paypal-button-container"></div>
-Dante Castelán Carpinteyro - Developer
 
 
 </html>
