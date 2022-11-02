72db4d6be3619ff70a048e1f22a32346169ea8af
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-02 00:18:04 -0600

    prueba de pagos

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prueba de pagos</title>
     <script src="https://www.paypal.com/sdk/js?client-id=AZfUU5uy4JNNgtbnwTOkZeicL8gO3f9GOZ03iRE1FKDVMeqB3DcUQ7Rm2QvNnF9rRiBNySF82QOEIGhB&currency=MXN"></script>
 </head>
 
