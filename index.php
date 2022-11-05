04b0cbc1a4352ef229d9ab1dfabcf66773cc21e7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-04 22:48:58 -0600

    ver

--- index.php
+++ index.php
@@ -22,6 +22,14 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">Catálogo</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </header>
