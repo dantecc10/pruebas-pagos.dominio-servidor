5e650c5133c94d1277df0d54e4c4d2a50db9bb16
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-04 22:45:49 -0600

    clase

--- index.php
+++ index.php
@@ -14,13 +14,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <header>
-        <div class="collapse bg-dark" id="navbarHeader">
-            <div class="container">
-                <div class="row">
-                </div>
-            </div>
-        </div>
-        <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="navbar navbar-dark bg-dark navbar-expand-lg">
             <div class="container">
                 <a href="#" class="navbar-brand d-flex align-items-center">
                     <strong>Tienda Online</strong>
