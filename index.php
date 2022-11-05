f36a157c879df7e73c703fae4fc641bf6fac7182
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-04 22:45:20 -0600

    cambios

--- index.php
+++ index.php
@@ -17,14 +17,6 @@
         <div class="collapse bg-dark" id="navbarHeader">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-4 offset-md-1 py-4">
-                        <h4 class="text-white">Contact</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                            <li><a href="#" class="text-white">Like on Facebook</a></li>
-                            <li><a href="#" class="text-white">Email me</a></li>
-                        </ul>
-                    </div>
                 </div>
             </div>
         </div>
