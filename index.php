85cb36cfad9b8471445f3a081b4e86038ed6ce42
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-04 22:46:12 -0600

    sin navegación

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <header>
         <div class="navbar navbar-dark bg-dark navbar-expand-lg">
             <div class="container">
-                <a href="#" class="navbar-brand d-flex align-items-center">
+                <a href="#" class="navbar-brand">
                     <strong>Tienda Online</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
