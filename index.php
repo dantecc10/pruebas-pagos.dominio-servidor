703c05ee5e1949a581f991ba5e01c3f61df64c2e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-05 22:25:49 -0600

    cambios

--- index.php
+++ index.php
@@ -38,6 +38,29 @@
         </div>
     </header>
 
+    <main>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="">
+                        <div class="card-body">
+                            <p class="card-title">Zapatos color café</p>
+                            <p class="card-text">$ 500.00</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
