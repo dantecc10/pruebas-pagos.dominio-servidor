f8ee0c068491e3d69108f8b8053b9a5b252c527d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-05 22:42:20 -0600

    integro estilos.css

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Tienda Online</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="css/estilos.css" rel="stylesheet">
 </head>
 
 <body>
