247a652ee36aaae25e6eb331bcacf6aecd703fac
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-06 09:16:30 -0600

    id get

--- details.php
+++ details.php
@@ -1,7 +1,11 @@
 <?php
+require 'config/config.php';
 require 'config/database.php';
 $db = new Database();
 $con = $db->conectar();
+
+$id = $_GET['id'];
+
 $sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
 $sql->execute();
 $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
