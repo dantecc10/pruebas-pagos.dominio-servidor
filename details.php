188be59cdbf104d78a7fbf3bd5817552207995e7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-06 23:38:17 -0600

    cosas

--- details.php
+++ details.php
@@ -145,7 +145,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                     </p>
                     <div class="d-grid gap-3 col-10 mx-auto">
                         <button class="btn btn-primary" type="button">Comprar ahora</button>
-                        <button class="btn btn-outline-primary" type="button">Agregar al carrito</button>
+                        <button class="btn btn-outline-primary" type="button" onclick="addProducto(<?php echo $id; ?>, '<?php echo $token_tmp; ?>')">Agregar al carrito</button>
                     </div>
                 </div>
 
