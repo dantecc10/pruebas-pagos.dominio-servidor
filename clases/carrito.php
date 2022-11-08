268fa300f98c247fb606d2149505df93d53bb403
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-08 07:28:18 -0600

    guión bajo

--- clases/carrito.php
+++ clases/carrito.php
@@ -5,10 +5,10 @@ if (isset($_POST['id'])) {
     $token = $_POST['token'];
 
     $token_tmp = hash_hmac('sha1', $id, KEY_TOKEN);
-    
+
     if ($token == $token_tmp) {
 
-        if (isset($SESSION['carrito']['productos'][$id])) {
+        if (isset($_SESSION['carrito']['productos'][$id])) {
             $_SESSION['carrito']['productos'][$id] += 1;
         } else {
             $_SESSION['carrito']['productos'][$id] = 1;
--- config/config.php
+++ config/config.php
@@ -5,6 +5,4 @@ session_start();
 $num_cart = 0;
 if (isset($_SESSION['carrito']['productos'])) {
     $num_cart = count($_SESSION['carrito']['productos']);
-} else {
-    # code...
 }
