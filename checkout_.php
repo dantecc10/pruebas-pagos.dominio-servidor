14ba9744410f05d0cc50efa88152363aff1cbc10
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-07 23:37:05 -0600

    crfeado nuevo checkout

--- checkout_.php
+++ checkout_.php
@@ -1,4 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba <?php
 require 'config/config.php';
 require 'config/database.php';
 $db = new Database();
@@ -111,5 +118,42 @@ print_r($_SESSION);
         }
     </script>
 </body>
+
+</html>de pagos</title>
+    <script src="https://www.paypal.com/sdk/js?client-id=AZfUU5uy4JNNgtbnwTOkZeicL8gO3f9GOZ03iRE1FKDVMeqB3DcUQ7Rm2QvNnF9rRiBNySF82QOEIGhB&currency=MXN"></script>
+</head>
+
+<body>
+    <div id="paypal-button-container"></div>
+    <script>
+        paypal.Buttons({
+            style: {
+                color: 'blue',
+                shape: 'pill',
+                label: 'pay'
+            },
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: 100
+                        }
+                    }]
+                });
+            },
+            onApprove: function(data, actions) {
+                actions.order.capture().then(function(detalles) {
+                    console.log(detalles);
+                    // window.location.href="/completado.html";
+                });
+            },
+            onCancel: function(data) {
+                alert("Pago cancelado");
+                console.log(data);
+            }
+        }).render('#paypal-button-container');
+    </script>
+</body>
+
 
 </html>
