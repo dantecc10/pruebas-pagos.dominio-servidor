d3a8020c37b2f79d295fc6a917505ec503435af2
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-08 21:38:34 -0600

    botoncito

--- checkout.php
+++ checkout.php
@@ -127,6 +127,11 @@ if ($productos != null) {
                 <?php } ?>
                 </table>
             </div>
+            <div class="row">
+                <div class="col-md-5 offset-md-7 d-grid gap-2">
+                    <button class="btn btn-lg btn-primary">Realizar pago</button>
+                </div>
+            </div>
         </div>
     </main>
     <script>
