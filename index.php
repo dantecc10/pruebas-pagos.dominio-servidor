eddcb97d7921b5c73a08207a5ebae296c5254300
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-02 16:45:55 -0600

    ..

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ print_r($_SESSION);
                                     <div class="btn-group">
                                         <a href="details.php?id=<?php echo $row['id']; ?>&token=<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>" class="btn btn-primary">Detalles</a>
                                     </div>
-                                    <button class="btn btn-success" type="button" onclick="addProducto(<?php echo $row['id']; ?>, '<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>')">Agregar al carrito</button>
+                                    <button class="btn btn-outline-success" type="button" onclick="addProducto(<?php echo $row['id']; ?>, '<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>')">Agregar al carrito</button>
                                 </div>
                             </div>
                         </div>
