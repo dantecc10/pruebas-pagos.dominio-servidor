0495f2d468de82b1ad89d4f8035fb1c9b69aa0cc
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-02 20:01:49 -0600

    ,,

--- details.php
+++ details.php
@@ -169,7 +169,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                 .then(data => {
                     if (data.ok) {
                         let elemento = document.getElementById("num_cart")
-                        elemento.innerHTML = data.número
+                        elemento.innerHTML = data.numero
                     }
                 })
         }
