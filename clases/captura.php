6a944fe4e46307d8a5b1749c3b163e5ee627bb5d
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-04 17:52:49 -0600

    ..

--- clases/captura.php
+++ clases/captura.php
@@ -8,8 +8,8 @@
     $json = file_get_contents('php://input');
     $datos = json_decode($json, true);
 
-    echo '<pre>';
-    print_r($datos);
+/*     echo '<pre>';
+    print_r($datos); */
 
     if (is_array($datos)) {
         $id_trasaccion = $datos['detalles']['id'];
