fdf8d722225e019a5007668a60656bfcc0a21bed
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-03 22:04:25 -0600

    Ajustes

--- config/config.php
+++ config/config.php
@@ -1,4 +1,7 @@
 <?php
+
+define("CLIENT_ID", "AZfUU5uy4JNNgtbnwTOkZeicL8gO3f9GOZ03iRE1FKDVMeqB3DcUQ7Rm2QvNnF9rRiBNySF82QOEIGhB");
+define("CURRENCY", "MXN");
 define("KEY_TOKEN", "APR.wqc-354*");
 define("MONEDA", "$");
 session_start();
