f5662faf5de497957721e9507cd42ffab295be89
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-04 20:20:03 -0600

    pruebas mail 1

---
+++ clases/enviar_email.php
@@ -0,0 +1,52 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../phpmailer/src/PHPMailer.php';
+require '../phpmailer//src/SMTP.php';
+require '../phpmailer/src/Exception.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //SMTP::DEBUG_OFF;                   //Enable verbose debug output
+    $mail->isSMTP();                                                             //Send using SMTP
+    $mail->Host       = 'prueba-pagos.castelancarpinteyro.club';                 //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                                    //Enable SMTP authentication
+    $mail->Username   = 'no-reply@castelancarpinteyro.club';                     //SMTP username
+    $mail->Password   = 'Y696zbii_q.5pYT';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('no-reply@castelancarpinteyro.club', 'Tienda online');
+    $mail->addAddress('jeremy.hdez9@gmail.com', 'User');     //Add a recipient
+/*     $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com'); */
+
+    //Envio de archivos
+/*     $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); */    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Detalles de compra';
+
+    $cuerpo = '<h4>Gracias por su compra</h4>';
+    $cuerpo .= '<p>El ID de su compra es <b>' . $idTransaccion . '</b></p>';
+
+    $mail->Body    = imap_utf8($cuerpo);
+    $mail->AltBody = 'Detalles de compra';
+
+    $mail->setLanguage('../phpmailer/language/phpmailer.lang-es.php');
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
