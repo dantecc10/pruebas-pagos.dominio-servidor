c4f90c04ebb923bebbbfcda0e671425271d6dccd
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-04 21:13:53 -0600

    ..

--- clases/enviar_email.php
+++ clases/enviar_email.php
@@ -15,7 +15,7 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //SMTP::DEBUG_OFF;                   //Enable verbose debug output
     $mail->isSMTP();                                                             //Send using SMTP
-    $mail->Host       = 'prueba-pagos.castelancarpinteyro.club';                 //Set the SMTP server to send through
+    $mail->Host       = 'mail.castelancarpinteyro.club';                 //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                                    //Enable SMTP authentication
     $mail->Username   = 'no-reply@castelancarpinteyro.club';                     //SMTP username
     $mail->Password   = 'Y696zbii_q.5pYT';                               //SMTP password
