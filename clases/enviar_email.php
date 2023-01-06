0b3bbbc905bb426e157a2ebf24221f65319c6c10
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-06 02:31:40 -0600

    autoload.php

--- clases/enviar_email.php
+++ clases/enviar_email.php
@@ -4,6 +4,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+require '../vendor/autoload.php';
+
 require '../phpmailer/src/PHPMailer.php';
 require '../phpmailer/src/SMTP.php';
 require '../phpmailer/src/Exception.php';
