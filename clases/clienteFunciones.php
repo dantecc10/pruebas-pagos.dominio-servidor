257a9d15388e829381059715fef72792e083278c
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-05 23:06:20 -0600

    ..

--- clases/clienteFunciones.php
+++ clases/clienteFunciones.php
@@ -6,7 +6,7 @@
 
   function registraCliente(array $datos,$con){
     $sql = $con->prepare("INSERT INTO clientes (nombres, apellidos, email, telefono, dni, estatus, fecha_alta) VALUES (?, ?, ?, ?, ?, 1, now())");
-    if($sql->execute([$datos])){
+    if($sql->execute($datos)){
         return $con->lastInsertId();
     }
     return 0;
@@ -14,7 +14,7 @@
 
   function registraUsuario(array $datos, $con){
     $sql = $con->prepare("INSERT INTO usuarios (usuario, password, token, id_cliente) VALUES (?, ?, ?, ?)");
-    if ($sql->execute([$datos])) {
+    if ($sql->execute($datos)) {
         return true;
     } 
     return false;
