ea89c9cbd52b7d2c4a88846aa17e9bae5ec46b44
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-05 22:23:36 -0600

    registro usuarios

---
+++ registro.php
@@ -0,0 +1,68 @@
+<?php
+/* Coonexion a db */
+require 'config/config.php';
+require 'config/database.php';
+$db = new Database();
+$con = $db->conectar();
+
+#session_destroy();
+#print_r($_SESSION);
+?>
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Online</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="css/estilos.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- Encabezado -->
+    <header>
+        <div class="navbar navbar-dark bg-dark navbar-expand-lg">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <strong>Tienda Online</strong>
+                </a>
+                <!-- Contenido de la navbar -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">Catálogo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Contacto</a>
+                        </li>
+                    </ul>
+                    <a href="checkout.php" class="btn btn-primary">
+                        Carrito <span id="num_cart" class="badge bg-secondary"><?php echo $num_cart; ?></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <h2>Datos del cliente</h2>
+            <form class="row g-3" action="registro.php" method="post" autocomplete="off">
+                <div class=" col-md-6">
+                    <label for="nombres"><span class="text-danger">*Nombres</span></label>
+                    <input type="text" name="nombres" id="nombres" class="form-control" required>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+
+</html>
