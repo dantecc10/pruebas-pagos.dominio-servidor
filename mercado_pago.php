838db2bb20e30d20b713dc7e240247ff083f565a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-07 01:35:15 -0600

    puro composer

--- mercado_pago.php
+++ mercado_pago.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 MercadoPago\SDK::setAccessToken('TEST-4230125490296814-010412-ebff25e83a6a07fc6c1ab235f63c4bef-557585605');
 
-//$preference = new MercadoPago\Preference;
+$preference = new MercadoPago\Preference;
 ?>
 <!DOCTYPE html>
 <html lang="es">
