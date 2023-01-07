ea8074eb73249a130a6bfab9c3ae132c0f44970d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-07 01:34:44 -0600

    genial

--- mercado_pago.php
+++ mercado_pago.php
@@ -4,12 +4,12 @@ require "vendor/autoload.php";
 
 //require "sdk-php";
 
-//MercadoPago\SDK::setAccesToken('');
+MercadoPago\SDK::setAccessToken('TEST-4230125490296814-010412-ebff25e83a6a07fc6c1ab235f63c4bef-557585605');
 
 //$preference = new MercadoPago\Preference;
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
